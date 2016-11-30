28d0237e24a3c3cbc3a9af5166cefdec5f023717
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2016-11-30 13:59:48 +0000

    start writing a multiboot header

---
+++ src/multiboot_header.asm
@@ -0,0 +1,16 @@
+section .multiboot_header
+header_start:
+	dd 0xe5250d6					; magic number for multiboot2 spec
+	dd 0						; Architecture 0 (i386)
+	dd header_end - header_start			; header length
+
+	; checksum	
+	dd 0x100000000 - (0xe8520d6 + 0 + (header_end - header_start))		; checksum
+	
+	; optional tags go here
+
+	; required end tag
+	dw 0						; type
+	dw 0						; flags
+	dw 8						; size
+header_end:
