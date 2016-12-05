a37f8b98bf2110f1fd3ef93907d978130086051f
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2016-12-05 14:02:05 +0000

    get bootable media generated, however it currently hangs instead of running

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rust-os
 An Operating System written in rust. Inspired by http://os.phil-opp.com/
+
+## pre-requisites
+- nasm
+- ld
+- binutils
+- grub
+- qemu
+
+## build steps
+nasm -f elf64 multiboot_header.asm
+nasm -f elf64 boot.asm
+ld -n -o bin/isofiles/kernel.bin -T src/linker.ld src/multiboot_header.o src/boot.o
+grub-mkrescue -o os.iso bin/isofiles
+
--- src/multiboot_header.asm
+++ src/multiboot_header.asm
@@ -1,12 +1,12 @@
 section .multiboot_header
 header_start:
-	dd 0xe5250d6					; magic number for multiboot2 spec
+	dd 0xe85250d6					; magic number for multiboot2 spec
 	dd 0						; Architecture 0 (i386)
 	dd header_end - header_start			; header length
 
-	; checksum	
-	dd 0x100000000 - (0xe8520d6 + 0 + (header_end - header_start))		; checksum
-	
+	; checksum
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))		; checksum
+
 	; optional tags go here
 
 	; required end tag
