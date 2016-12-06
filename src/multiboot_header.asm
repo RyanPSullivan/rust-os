48bd59e110740cc60392c6a45a69521ce4ae16ac
Ryan Sullivan <ryan@bossalien.com>
2016-12-06 14:02:10 +0000

    fix various issues with files

--- src/multiboot_header.asm
+++ src/multiboot_header.asm
@@ -1,16 +1,15 @@
 section .multiboot_header
 header_start:
-	dd 0xe85250d6					; magic number for multiboot2 spec
-	dd 0						; Architecture 0 (i386)
-	dd header_end - header_start			; header length
+    dd 0xe85250d6                ; magic number (multiboot 2)
+    dd 0                         ; architecture 0 (protected mode i386)
+    dd header_end - header_start ; header length
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
-	; checksum
-	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))		; checksum
+    ; insert optional multiboot tags here
 
-	; optional tags go here
-
-	; required end tag
-	dw 0						; type
-	dw 0						; flags
-	dw 8						; size
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
 header_end:
