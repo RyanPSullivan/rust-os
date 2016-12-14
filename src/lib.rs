e564611b29d05a3003e2a53481775e1cd5cb5a16
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2016-12-14 14:12:22 +0000

    actually get rust printing hello world to the VGA buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,44 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
-pub extern fn rust_main() 
+pub extern "C"  fn rust_main() 
 {
-  let x = ["Hello", "World", "!"];
-  let y = x;
+    // ATTENTION: we have a very small stack and no guard page
+
+    let hello = b"Hello World!";
+    let color_byte = 0x1f; // white foreground, blue background
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // write `Hello World!` to the center of the VGA text buffer
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop{} 
 }
 
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
+#[lang = "eh_personality"] 
+extern "C" fn eh_personality() {}
+
+#[lang = "panic_fmt"] 
+#[no_mangle]
+pub extern "C" fn panic_fmt() -> ! {
+    let buffer_ptr = (0xb8000) as *mut _;
+    let red = 0x4f;
+    unsafe {
+        *buffer_ptr = [b'P', red, b'a', red, b'n', red, b'i', red, b'c', red, b'!', red];
+    };
+    loop { }
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {
+    loop {}
+}
